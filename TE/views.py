7ab687e2adebd28c1a0c3944f3cf6e3f39a44af4
Ravi Kuntumalla <49606689+ravihere@users.noreply.github.com>
2020-09-04 11:14:00 +0530

    Update views.py

--- TE/views.py
+++ TE/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from PIL import Image
 import pytesseract
-import cv2
 from .forms import reqData
 def index(request):
 	return render(request,'TE/index.html')
@@ -21,18 +20,10 @@ def get_text(request):
         if request.FILES.get("image", None) is not None:
             image = request.FILES["image"]
         try:
-            image = cv2.imread(image)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            blur = cv2.GaussianBlur(gray, (3,3), 0)
-            thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-            # Morph open to remove noise and invert image
-            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-            opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
-            invert = 255 - opening
-            pytesseract.pytesseract.tesseract_cmd ='/app/.apt/usr/bin/tesseract'
-            result = pytesseract.image_to_string(invert, lang=lang,config='--psm 6')
-            result= result.replace('\n\n','\n')
+            img = Image.open(image)
+	    pytesseract.pytesseract.tesseract_cmd ='/app/.apt/usr/bin/tesseract'
+            result = pytesseract.image_to_string(img, lang=lang, config='--psm 6')
+            result= result.replace('\n\n','\n')z
             context={
             'result':result,
             }
