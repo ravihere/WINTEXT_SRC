b5c2d2679f6bd3a85cf7cf7984216ed539822159
Ravi Kuntumalla <49606689+ravihere@users.noreply.github.com>
2020-09-04 11:21:30 +0530

    Update views.py

--- TE/views.py
+++ TE/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from PIL import Image
 import pytesseract
+import cv2
 from .forms import reqData
 def index(request):
 	return render(request,'TE/index.html')
@@ -18,14 +19,21 @@ def get_text(request):
             return render(request,'TE/WinText.html')
             # check to see if an image was uploaded
         if request.FILES.get("image", None) is not None:
-            image = request.FILES["image"]
+            img = request.FILES["image"]
         try:
-            img = Image.open(image)
-	    pytesseract.pytesseract.tesseract_cmd ='/app/.apt/usr/bin/tesseract'
-            result = pytesseract.image_to_string(img, lang=lang, config='--psm 6')
-            result= result.replace('\n\n','\n')z
+            i = Image.open(img)
+            image = cv2.imread(i)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            blur = cv2.GaussianBlur(gray, (3,3), 0)
+            thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+            opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
+            invert = 255 - opening
+            pytesseract.pytesseract.tesseract_cmd ='/app/.apt/usr/bin/tesseract'
+            result = pytesseract.image_to_string(invert, lang=lang, config='--psm 6')
+            result= result.replace('\n\n','\n')
             context={
-            'result':result,
+                'result':result,
             }
         except:
             return render(request, 'TE/WinText.html')
